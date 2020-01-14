5ef513c79e5594f371a2163859c72ecc1c4f8586
almas <mendygax@gmail.com>
2020-01-13 22:37:36 -0800

    Basic HomePage Tests added

--- src/test/java/HomePage.java
+++ src/test/java/HomePage.java
@@ -1,18 +1,43 @@
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class HomePage {
+public class HomePage extends BaseTest{
 
-    public WebDriver driver;
 
     @Test
-    public void setUp(){
-        driver = new ChromeDriver();
-        driver.get("https://www.domuso.com/");
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    public void navBarTest(){
+        driver.findElement(By.xpath("//a[@class='Header-nav-folder-title'][contains(text(),'Property Managers')]")).click();
+        WebElement title = driver.findElement(By.xpath("//h1[contains(text(),'Explore the Domuso Difference')]"));
+        Assert.assertTrue(title.isDisplayed());
+
+        driver.findElement(By.xpath("//a[@class='Header-nav-folder-title'][contains(text(),'Residents')]")).click();
+        WebElement title2 = driver.findElement(By.xpath("//h1[contains(text(), 'Resident Hub')]"));
+        Assert.assertTrue(title2.isDisplayed());
+
+        driver.findElement(By.xpath("//a[@class='Header-nav-folder-title'][contains(text(),'About')]")).click();
+        WebElement title3 = driver.findElement(By.xpath("//p[contains(text(),'Get to know us.')]"));
+        Assert.assertTrue(title3.isDisplayed());
+
     }
+
+    @Test
+    public void UITest(){
+        WebElement logo = driver.findElement(By.cssSelector("[class='Header-branding']"));
+        Assert.assertTrue(logo.isDisplayed());
+
+        WebElement wallPaper = driver.findElement(By.cssSelector("[id='block-yui_3_17_2_1_1559595529751_6808']"));
+        Assert.assertTrue(wallPaper.isDisplayed());
+
+        WebElement header = driver.findElement(By.cssSelector("[id='block-yui_3_17_2_1_1559598408625_34374']"));
+        Assert.assertTrue(header.isDisplayed());
+    }
+
+
 
 }
